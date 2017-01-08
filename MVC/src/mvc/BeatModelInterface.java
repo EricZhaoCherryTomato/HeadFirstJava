a4f034bb1f08db06362f57605fd5239425e3498b
zhaoye <eric.ye.zhao@gmail.com>
2017-01-08 21:40:31 +1000

    Add pattern summary note

---
+++ MVC/src/mvc/BeatModelInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+/**
+ *
+ * @author zhaoye
+ */
+public interface BeatModelInterface {
+    void initaliz();
+    void on();
+    void off();
+    void setBMP(int bmp);
+    int getBMP();
+    void registerObserver(BeatObserver o);
+    void removeObserver(BeatObserver o);
+    void registerObserver(BPMObserver o);
+    void removeObserver(BPMObserver o);
+}
